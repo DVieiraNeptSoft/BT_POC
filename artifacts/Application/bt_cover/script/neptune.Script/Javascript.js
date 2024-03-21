17008ffec5ae5c98dd4a8d85bf7a7464345b3166
Daniel Vieira <daniel.vieira@neptune-software.com>
2024-03-21 14:41:46 +0000

    new commit

---
+++ artifacts/Application/bt_cover/script/neptune.Script/Javascript.js
@@ -0,0 +1,20 @@
+var sFormattedTextContentBT =
+    "<p><h4 style=' font-size: 24px!important; color: #5514b4!important; padding: 0 0.5rem;'>Your broadband offers</h4></p>";
+FormattedTextBT.setHtmlText(sFormattedTextContentBT);
+
+var sFormattedTextContentBT2 =
+    "<p><h4 style=' font-size: 24px!important; color: #5514b4!important; padding: 0 0.5rem;'>Upgrade or switch your TV</h4></p>";
+FormattedTextBT2.setHtmlText(sFormattedTextContentBT2);
+
+var sFormattedTextContentBT3 =
+    "<p><h4 style=' font-size: 24px!important; color: #5514b4!important; padding: 0 0.5rem;'>Log in to manage My BT</h4></p>";
+FormattedTextBT3.setHtmlText(sFormattedTextContentBT3);
+
+
+if (sap.ui.Device.system.desktop) {
+    // Code for desktop devices
+} else if (sap.ui.Device.system.tablet) {
+    // Code for tablet devices
+} else if (sap.ui.Device.system.phone) {
+    // Code for phone devices
+}
